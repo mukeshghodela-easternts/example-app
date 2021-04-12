f17b5df8acd223011610d150712fb6922d143dc2
Mukesh <mukesh.easternts@gmail.com>
2021-04-12 08:21:51 +0530

    Conditions inblade template

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     function loadView() {
-        return view('posts', ["posts"=>["Post 1", "Post 2", "Post 3"]]);
+        return view('posts', ["posts"=>["Post 1", "Post 2", "Post 3", "Post 4", "Post 5", "Post 6", "Post 7"]]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,2 +1,13 @@
 <h1> Posts </h1>
 <h2>Total Posts : {{ count($posts) }}</h2>
+
+@if (count($posts) > 5)
+    <h2>5+ Posts</h2>
+
+@elseif(count($posts) > 10)
+    <h2>10+ Posts </h2>
+
+@else
+    <h2>{{ count($posts) }} Posts</h2>
+
+@endif
